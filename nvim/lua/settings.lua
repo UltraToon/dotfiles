c8e88256fbbaed7ceb96477b12179229579d0b7c
Ivan <92064752+UltraToon@users.noreply.github.com>
2022-03-16 22:45:54 -0700

    Add files via upload

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,22 @@
+--------------
+-- Settings --
+--------------
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.showmode = false
+vim.opt.wrap = false
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.incsearch = true
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.showtabline = 2
+vim.opt.smartindent = true
+
